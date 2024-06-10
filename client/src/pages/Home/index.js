0c09e3bccacb2b30484a28e8c6021920552202fb
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-06-10 10:47:01 +0530

    Beautify navbar

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,42 +1,34 @@
-import React from "react";
+import React, { Suspense, lazy, useMemo } from "react";
 import Navbar from "../../components/Navbar";
-import Discover from "../../components/Discover";
-import Vision from "../../components/Vision";
-import Features from "../../components/Features";
-import Team from "../../components/Team/Team";
-import Pricing from "../../components/Pricing";
-import Faq from "../../components/Faq";
-import Contact from "../../components/Contact";
-import Footer from "../../components/Footer";
 import { useSelector } from "react-redux";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const Discover = lazy(() => import("../../components/Discover"));
+const Vision = lazy(() => import("../../components/Vision"));
+const Features = lazy(() => import("../../components/Features"));
+const Team = lazy(() => import("../../components/Team/Team"));
+const Pricing = lazy(() => import("../../components/Pricing"));
+const Faq = lazy(() => import("../../components/Faq"));
+const Contact = lazy(() => import("../../components/Contact"));
+const Footer = lazy(() => import("../../components/Footer"));
 
 function Home() {
   const { skillnaavData } = useSelector((state) => state.root);
 
+  const memoizedData = useMemo(() => skillnaavData, [skillnaavData]);
+
   return (
     <div className="font-inter">
       <Navbar />
-      {/* {skillnaavData && (
-        <>
-          <Discover />
-          <div className="container mx-auto px-4 lg:px-20">
-            <Vision />
-          </div>
-          <div className="container mx-auto px-4 lg:px-20">
-            <Features />
-          </div>
-          <div className="container mx-auto px-4 lg:px-20">
-            <Team />
-            <Pricing />
-            <Faq />
-            <Contact />
-            <Footer />
-          </div>
-        </>
-      )} */}
-      {skillnaavData && (
-        <>
-        
+
+      {memoizedData ? (
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center h-screen">
+              <ClipLoader size={50} color={"#4328EB"} />
+            </div>
+          }
+        >
           <Discover />
           <div className="px-[20px] lg:px-20 mx-auto">
             <Vision />
@@ -47,7 +39,11 @@ function Home() {
             <Contact />
             <Footer />
           </div>
-        </>
+        </Suspense>
+      ) : (
+        <div className="flex items-center justify-center h-screen">
+          <ClipLoader size={50} color={"#4328EB"} />
+        </div>
       )}
     </div>
   );
