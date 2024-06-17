008bc11cad70e0963eab81816e449bd9e9823482
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-06-17 16:26:24 +0530

    Push

--- routes/skillnaavRoute.js
+++ routes/skillnaavRoute.js
@@ -16,398 +16,135 @@ const {
 
 const User = require("../models/userModel");
 
-// get all SkillNaav data
+// Helper function to handle CRUD operations
+const handleCRUD = (Model, action) => async (req, res) => {
+  try {
+    let result;
+    switch (action) {
+      case "getAll":
+        result = await Model.find();
+        break;
+      case "findByIdAndUpdate":
+        result = await Model.findByIdAndUpdate(req.body._id, req.body, {
+          new: true,
+        });
+        break;
+      case "create":
+        result = new Model(req.body);
+        await result.save();
+        break;
+      case "findByIdAndDelete":
+        await Model.findByIdAndDelete(req.params.id);
+        result = {
+          success: true,
+          message: `${Model.modelName} deleted successfully`,
+        };
+        break;
+      default:
+        return res.status(400).send({ message: "Invalid action" });
+    }
+    res.status(200).send({ data: result, success: true });
+  } catch (error) {
+    console.error(`Error with ${Model.modelName} ${action}:`, error);
+    res
+      .status(500)
+      .send({ message: `Error with ${Model.modelName} ${action}`, error });
+  }
+};
+
+// Get all SkillNaav data
 router.get("/get-skillnaav-data", async (req, res) => {
   try {
-    const discovers = await Discover.find();
-    const visionhead = await VisionHead.find();
-    const visionpoint = await VisionPoint.find();
-    const features = await Feature.find();
-    const team = await Team.find();
-    const teammember = await TeamMember.find();
-    const pricing = await Pricing.find();
-    const pricingcard = await PricingCard.find();
-    const faq = await FAQ.find();
-    const faqcard = await FAQCard.find();
-    const contact = await Contact.find();
-    const footer = await Footer.find();
+    const data = await Promise.all([
+      Discover.find(),
+      VisionHead.find(),
+      VisionPoint.find(),
+      Feature.find(),
+      Team.find(),
+      TeamMember.find(),
+      Pricing.find(),
+      PricingCard.find(),
+      FAQ.find(),
+      FAQCard.find(),
+      Contact.find(),
+      Footer.find(),
+    ]);
 
     res.status(200).send({
-      discover: discovers,
-      visionhead: visionhead,
-      visionpoint: visionpoint,
-      features: features,
-      team: team,
-      teammember: teammember,
-      pricing: pricing,
-      pricingcard: pricingcard,
-      faq: faq,
-      faqcard: faqcard,
-      contact: contact,
-      footer: footer,
+      discover: data[0],
+      visionhead: data[1],
+      visionpoint: data[2],
+      features: data[3],
+      team: data[4],
+      teammember: data[5],
+      pricing: data[6],
+      pricingcard: data[7],
+      faq: data[8],
+      faqcard: data[9],
+      contact: data[10],
+      footer: data[11],
     });
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-// Update discover
-router.post("/update-discover", async (req, res) => {
-  try {
-    const discover = await Discover.findOneAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: discover,
-      success: true,
-      message: "Discover updated successfully",
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+// CRUD routes
+router.post("/update-discover", handleCRUD(Discover, "findByIdAndUpdate"));
+router.post(
+  "/update-visionheading",
+  handleCRUD(VisionHead, "findByIdAndUpdate")
+);
+router.post("/add-visionpoint", handleCRUD(VisionPoint, "create"));
+router.post(
+  "/update-visionpoint",
+  handleCRUD(VisionPoint, "findByIdAndUpdate")
+);
+router.delete(
+  "/delete-visionpoint/:id",
+  handleCRUD(VisionPoint, "findByIdAndDelete")
+);
+router.post("/update-feature", handleCRUD(Feature, "findByIdAndUpdate"));
+router.post("/add-feature", handleCRUD(Feature, "create"));
+router.delete("/delete-feature/:id", handleCRUD(Feature, "findByIdAndDelete"));
+router.post("/update-teamheading", handleCRUD(Team, "findByIdAndUpdate"));
+router.post("/add-teammember", handleCRUD(TeamMember, "create"));
+router.post("/update-teammember", handleCRUD(TeamMember, "findByIdAndUpdate"));
+router.delete(
+  "/delete-teammember/:id",
+  handleCRUD(TeamMember, "findByIdAndDelete")
+);
+router.post("/update-priceheading", handleCRUD(Pricing, "findByIdAndUpdate"));
+router.post("/add-pricingcard", handleCRUD(PricingCard, "create"));
+router.post(
+  "/update-pricingcard",
+  handleCRUD(PricingCard, "findByIdAndUpdate")
+);
+router.delete(
+  "/delete-pricingcard/:id",
+  handleCRUD(PricingCard, "findByIdAndDelete")
+);
+router.post("/update-faqheading", handleCRUD(FAQ, "findByIdAndUpdate"));
+router.post("/add-faqcard", handleCRUD(FAQCard, "create"));
+router.post("/update-faqcard", handleCRUD(FAQCard, "findByIdAndUpdate"));
+router.delete("/delete-faqcard/:id", handleCRUD(FAQCard, "findByIdAndDelete"));
 
-// Update Vision Heading
-router.post("/update-visionheading", async (req, res) => {
-  try {
-    const visionhead = await VisionHead.findByIdAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: visionhead,
-      success: true,
-      message: "Vision Heading updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating Vision Heading:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Add Vision Point
-router.post("/add-visionpoint", async (req, res) => {
-  try {
-    const visionpoint = new VisionPoint(req.body);
-    await visionpoint.save();
-    res.status(200).send({
-      data: visionpoint,
-      success: true,
-      message: "Feature added successfully",
-    });
-  } catch (error) {
-    console.error("Error adding vision:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Update Vision Point
-router.post("/update-visionpoint", async (req, res) => {
-  try {
-    const visionpoint = await VisionPoint.findOneAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: visionpoint,
-      success: true,
-      message: "Vision Point updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating vision point:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Delete Vision Point
-router.delete("/delete-visionpoint/:id", async (req, res) => {
-  try {
-    await VisionPoint.findByIdAndDelete(req.params.id);
-    res.status(200).send({
-      success: true,
-      message: "Vision Point deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Update Feature
-router.post("/update-feature", async (req, res) => {
-  try {
-    console.log("Updating feature with data:", req.body);
-    const feature = await Feature.findOneAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    console.log("Feature updated:", feature);
-    res.status(200).send({
-      data: feature,
-      success: true,
-      message: "Feature updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating feature:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Add Feature
-router.post("/add-feature", async (req, res) => {
-  try {
-    console.log("Adding feature with data:", req.body);
-    const feature = new Feature(req.body);
-    await feature.save();
-    console.log("Feature added:", feature);
-    res.status(200).send({
-      data: feature,
-      success: true,
-      message: "Feature added successfully",
-    });
-  } catch (error) {
-    console.error("Error adding feature:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Delete Feature
-router.delete("/delete-feature/:id", async (req, res) => {
-  try {
-    console.log("Deleting feature with ID:", req.params.id);
-    await Feature.findByIdAndDelete(req.params.id);
-    console.log("Feature deleted");
-    res.status(200).send({
-      success: true,
-      message: "Feature deleted successfully",
-    });
-  } catch (error) {
-    console.error("Error deleting feature:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Update Team Heading
-router.post("/update-teamheading", async (req, res) => {
-  try {
-    const teamhead = await Team.findByIdAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: teamhead,
-      success: true,
-      message: "Team Heading updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating Team Heading:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Add Team Member
-router.post("/add-teammember", async (req, res) => {
-  try {
-    const teammember = new TeamMember(req.body);
-    await teammember.save();
-    res.status(200).send({
-      data: teammember,
-      success: true,
-      message: "Team Member added successfully",
-    });
-  } catch (error) {
-    console.error("Error adding team member:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Update Team Member
-router.post("/update-teammember", async (req, res) => {
-  try {
-    const teammember = await TeamMember.findByIdAndUpdate(
-      req.body._id,
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: teammember,
-      success: true,
-      message: "Team member updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating team member:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Delete Team Member
-router.delete("/delete-teammember/:id", async (req, res) => {
-  try {
-    await TeamMember.findByIdAndDelete(req.params.id);
-    res.status(200).send({
-      success: true,
-      message: "Team Member deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Update Price Heading
-router.post("/update-priceheading", async (req, res) => {
-  try {
-    const pricing = await Pricing.findByIdAndUpdate(
-      { _id: req.body._id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: pricing,
-      success: true,
-      message: "Price Heading updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating Price Heading:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Add Price Card
-router.post("/add-pricingcard", async (req, res) => {
-  try {
-    const pricingcard = new PricingCard(req.body);
-    await pricingcard.save();
-    res.status(200).send({
-      data: pricingcard,
-      success: true,
-      message: "Price Card added successfully",
-    });
-  } catch (error) {
-    console.error("Error adding Price Card :", error);
-    res.status(500).send(error);
-  }
-});
-
-// Update Price Card
-router.post("/update-pricingcard", async (req, res) => {
-  try {
-    const pricingcard = await PricingCard.findByIdAndUpdate(
-      req.body._id,
-      req.body,
-      { new: true }
-    );
-    res.status(200).send({
-      data: pricingcard,
-      success: true,
-      message: "Pricing Card updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating Pricing Card:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Delete Price Card
-router.delete("/delete-pricingcard/:id", async (req, res) => {
-  try {
-    await PricingCard.findByIdAndDelete(req.params.id);
-    res.status(200).send({
-      success: true,
-      message: "Pricing Card deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// Update FAQ Heading
-router.post("/update-faqheading", async (req, res) => {
-  try {
-    const faq = await FAQ.findByIdAndUpdate({ _id: req.body._id }, req.body, {
-      new: true,
-    });
-    res.status(200).send({
-      data: faq,
-      success: true,
-      message: "FAQ Heading updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating FAQ Heading:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Add FAQ Card
-router.post("/add-faqcard", async (req, res) => {
-  try {
-    const faqcard = new FAQCard(req.body);
-    await faqcard.save();
-    res.status(200).send({
-      data: faqcard,
-      success: true,
-      message: "FAQ Card added successfully",
-    });
-  } catch (error) {
-    console.error("Error adding FAQ Card :", error);
-    res.status(500).send(error);
-  }
-});
-
-// Update FAQ Card
-router.post("/update-faqcard", async (req, res) => {
-  try {
-    const faqcard = await FAQCard.findByIdAndUpdate(req.body._id, req.body, {
-      new: true,
-    });
-    res.status(200).send({
-      data: faqcard,
-      success: true,
-      message: "FAQ Card updated successfully",
-    });
-  } catch (error) {
-    console.error("Error updating FAQ Card:", error);
-    res.status(500).send(error);
-  }
-});
-
-// Delete FAQ Card
-router.delete("/delete-faqcard/:id", async (req, res) => {
-  try {
-    await FAQCard.findByIdAndDelete(req.params.id);
-    res.status(200).send({
-      success: true,
-      message: "FAQ Card deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//admin login
+// Admin login
 router.post("/admin-login", async (req, res) => {
   try {
     const user = await User.findOne({
       username: req.body.username,
       password: req.body.password,
     });
-    user.password = "";
     if (user) {
-      res.status(200).send({
-        data: user,
-        success: true,
-        message: "Login Successfully",
-      });
+      user.password = "";
+      res
+        .status(200)
+        .send({ data: user, success: true, message: "Login Successfully" });
     } else {
-      res.status(200).send({
-        data: user,
-        success: false,
-        message: "Invalid username or password",
-      });
+      res
+        .status(200)
+        .send({ success: false, message: "Invalid username or password" });
     }
   } catch (error) {
     res.status(500).send(error);
@@ -448,13 +185,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    await Contact.findByIdAndDelete(req.params.id);
-    res.status(200).send({ message: "Contact deleted successfully" });
-  } catch (error) {
-    res.status(500).send({ message: "Error deleting contact", error });
-  }
-});
+router.delete("/:id", handleCRUD(Contact, "findByIdAndDelete"));
 
 module.exports = router;
