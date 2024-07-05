110b9bf8c10a8088624bf828464a2dbdab9fb2f8
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-07-05 15:15:02 +0530

    Prod-main live

--- client/src/components/Vision.js
+++ client/src/components/Vision.js
@@ -33,12 +33,12 @@ const Vision = () => {
             <h2 className="text-3xl lg:text-4xl font-bold mb-4 lg:mb-6 text-center">
               {visionhead[0].visionheading}
             </h2>
-            <p className="text-lg lg:text-xl mb-6 max-w-3xl mx-auto">
+            <p className="text-lg lg:text-xl font-medium mb-6 max-w-3xl mx-auto">
               {visionhead[0].visionsub}
             </p>
             <ul className="list-disc text-base lg:text-lg max-w-2xl mx-auto pl-5">
               {visionpoint.map((point, index) => (
-                <li key={index} className="mb-3">
+                <li key={index} className="mb-3 font-normal">
                   {point.visionpoint}
                 </li>
               ))}
